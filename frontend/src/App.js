25a105df1267cb170a4a75eeeeb1e53d3bdf03ef
relier54 <i.curipan996@gmail.com>
2022-08-10 02:52:06 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
